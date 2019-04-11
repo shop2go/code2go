6ababa51073b31c54418664906c1a9f255abd557
mmaedel <medienwerksalzburg@gmail.com>
2019-04-11 17:35:56 +0200

    nav

--- now/index.go
+++ now/index.go
@@ -193,7 +193,7 @@ func Handler(w http.ResponseWriter, r *http.Request) {
     <button class="btn btn-outline-light my-2 my-sm-1" type="submit">Search</button>
   </form><br>
   <form class="form-inline" role="form" method="get">
-					` + strconv.Itoa(c.Year) + ` -> <button id="nav" type="submit" class="btn btn-link" onclick="window.location.href='prev'" value="` + strconv.Itoa(c.Month) + `">` + now.AddDate(0, -1, 0).Month().String() + `</button>` + now.Month().String() + `<button type="button" class="btn btn-link" onclick="window.location.href='next'">` + now.AddDate(0, +1, 0).Month().String() + `</button>
+					` + strconv.Itoa(c.Year) + ` -> <button class="btn btn-link" onclick="window.location.href='prev'">` + now.AddDate(0, -1, 0).Month().String() + `</button>` + now.Month().String() + `<button type="button" class="btn btn-link" onclick="window.location.href='next'">` + now.AddDate(0, +1, 0).Month().String() + `</button>
 					</form>
 					</div>
 					<div class="container" id="data" style="color:white;">
@@ -534,17 +534,10 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if r.Method == "GET" {
-		
 
 		w.Header().Set("Content-Type", "text/html")
 		w.Header().Set("Content-Length", strconv.Itoa(len(str)))
 		w.Write([]byte(str))
-
-		r.ParseForm()
-
-		s := strings.Join(r.Form["nav"], " ")
-
-		fmt.Fprint(w, s)
 
 	} else {
 
