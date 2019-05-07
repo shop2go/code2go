f784eb6a1aa5b4f194977224df13a750ad3bb376
mmaedel <medienwerksalzburg@gmail.com>
2019-05-07 12:02:01 +0200

    urlnav

--- entry/index.go
+++ entry/index.go
@@ -16,6 +16,29 @@ type Cal struct {
 
 func Handler(w http.ResponseWriter, r *http.Request) {
 
+	str := `
+
+	<!DOCTYPE html>
+	<html lang="en">
+		 <head>
+				<meta charset="UTF-8">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				<meta http-equiv="X-UA-Compatible" content="ie=edge">
+				<title>CODE2GO</title>
+				<!-- CSS -->
+				<!-- Add Material font (Roboto) and Material icon as needed -->
+				<link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700,700i|Roboto+Mono:300,400,700|Roboto+Slab:300,400,700" rel="stylesheet">
+				<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+				<!-- Add Material CSS, replace Bootstrap CSS -->
+				<link href="https://assets.medienwerk.now.sh/material.min.css" rel="stylesheet">
+				</head>
+				<body style="background-color: #bcbcbc;">
+
+				<div class="container" id="data" style="color:white; font-size:30px;">
+				<ul class="list-group">
+`
+
 	now := time.Now()
 
 	var c Cal
@@ -83,29 +106,6 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 		q = i
 
 	}
-
-	str := `
-
-		<!DOCTYPE html>
-		<html lang="en">
-			 <head>
-					<meta charset="UTF-8">
-					<meta name="viewport" content="width=device-width, initial-scale=1.0">
-					<meta http-equiv="X-UA-Compatible" content="ie=edge">
-					<title>CODE2GO</title>
-					<!-- CSS -->
-					<!-- Add Material font (Roboto) and Material icon as needed -->
-					<link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700,700i|Roboto+Mono:300,400,700|Roboto+Slab:300,400,700" rel="stylesheet">
-					<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-
-					<!-- Add Material CSS, replace Bootstrap CSS -->
-					<link href="https://assets.medienwerk.now.sh/material.min.css" rel="stylesheet">
-					</head>
-					<body style="background-color: #bcbcbc;">
-
-					<div class="container" id="data" style="color:white; font-size:30px;">
-					<ul class="list-group">
- `
 
 	for k := q; k <= l; k++ {
 
