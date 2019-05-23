ae4bbc97c3ba5347a26cf10b397374c425723580
mmaedel <medienwerksalzburg@gmail.com>
2019-05-23 20:32:51 +0200

    urlnav

--- form/index.go
+++ form/index.go
@@ -41,7 +41,7 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 		<input class="form-control mr-sm-2" type="text" placeholder="topic" aria-label="Topic" id ="Topic" name ="Topic" required><br>
 		<input class="form-control mr-sm-2" type="text" placeholder="event" aria-label="Event" id ="Event" name ="Event" required><br>
 		<input class="form-control mr-sm-2" type="text" placeholder="tag" aria-label="Tag" id ="Tag" name ="Tag"><br>
-		<input class="form-control mr-sm-2" type="text" placeholder="` + strings.Replace(url, "form", "", -1) +`" aria-label="Date" id ="Date" name ="Date" value="` + strings.Replace(url, "form", "", -1) `" readonly><br>
+		<input class="form-control mr-sm-2" type="text" placeholder="` + strings.Replace(url, "form", "", -1) + `" aria-label="Date" id ="Date" name ="Date" value="` + strings.Replace(url, "form", "", -1) + `" readonly><br>
 
 		<button class="btn btn-outline-light my-2 my-sm-1" type="submit">set</button><br>
 	  </div>
