fa9f1090c8940788e1135816a4ff7def42137c63
mmaedel <medienwerksalzburg@gmail.com>
2019-12-11 09:17:44 +0100

    urlnav

--- entry/index.go
+++ entry/index.go
@@ -50,13 +50,15 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 
 	var result []Cache = make([]Cache, 0)
 
-	url := strings.TrimPrefix(r.URL.Path, "/entry#")
+	url := strings.TrimPrefix(r.URL.Path, "/")
+
+	url = strings.TrimPrefix(url,"entry#")
 
 	sl := strings.SplitN(url, "-", -1)
 
 	fc := f.NewFaunaClient(os.Getenv("FAUNA_ACCESS"))
 
-	x, err := fc.Query(f.CreateKey(f.Obj{"database": f.Database(sl[0]), "role": "server-readonly"}))
+	x, err := fc.Query(f.CreateKey(f.Obj{"database": f.Database(sl[0]), "role": "server"}))
 
 	if err != nil {
 
