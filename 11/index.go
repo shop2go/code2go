d071fde943c3a63fae559e336413f19ffa391fe1
mmaedel <medienwerksalzburg@gmail.com>
2020-01-01 16:35:49 +0100

    urlnav

--- 11/index.go
+++ 11/index.go
@@ -2,24 +2,17 @@ package main
 
 import (
 	"context"
-	//"encoding/json"
 	"fmt"
-	//"io/ioutil"
 	"net/http"
 	"os"
-
-	//"sort"
 	"strconv"
 	"strings"
 	"time"
-
-	//"github.com/mschneider82/problem"
 
 	"golang.org/x/oauth2"
 
 	f "github.com/fauna/faunadb-go/faunadb"
 	"github.com/shurcooL/graphql"
-	//ms "github.com/mitchellh/mapstructure"
 )
 
 type Cal struct {
@@ -35,41 +28,8 @@ type Access struct {
 	Role      string `fauna:"role"`
 }
 
-type Post struct {
-	ID graphql.String `graphql:"_id"`
-}
-
-var cache []graphql.String = make([]graphql.String, 0)
-
-/* func response(w http.ResponseWriter, success bool, message string, method string) {
-	// Create a map for the response body
-	body := make(map[string]interface{})
-
-	// Prepare the return data
-	if success {
-		body["type"] = "connected"
-	} else {
-		body["type"] = "failure"
-	}
-	body["message"] = message
-
-	bodyString, _ := json.Marshal(body)
-
-	// Return the response
-	if method == http.MethodOptions {
-		w.Header().Set("Access-Control-Allow-Headers", "*")
-	}
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Credentials", "true")
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(bodyString)
-
-} */
-
 func Handler(w http.ResponseWriter, r *http.Request) {
 
-	//var id f.RefV
 	url := strings.TrimPrefix(r.URL.Path, "/")
 
 	n, err := strconv.Atoi(url)
@@ -85,8 +45,6 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	now := time.Now().AddDate(0, n, 0)
 
 	var c Cal
-
-	//c.Year = now.Year()
 
 	c.Month = int(now.Month())
 	day := map[int]string{now.Day(): now.Weekday().String()}
@@ -261,23 +219,6 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 
 		}
 
-		/* 			str = str + `
-				<br>
-				` + time.Now().AddDate(0, t, 0).Format("2006") + `
-				<br>
-				<button type="button" class="btn btn-outline-dark" onclick="window.location.href='` + strconv.Itoa(t) + `'">
-				` + time.Now().AddDate(0, t, 0).Format("Jan") + `
-				</button>
-				`
-			c.Year = time.Now().AddDate(0, t, 0).Year()
-		} else {
-			str = str + `
-				<button type="button" class="btn btn-outline-dark" onclick="window.location.href='` + strconv.Itoa(t) + `'">
-				` + time.Now().AddDate(0, t, 0).Format("Jan") + `
-				</button>
-				`
-		} */
-				
 		c.Year = y
 
 	}
@@ -452,25 +393,49 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 
 	call := graphql.NewClient("https://graphql.fauna.com/graphql", httpClient)
 
-	value := now.Format("2006-01-02")
+	value := now.Format("2006-01")
 
 	mo := fmt.Sprintf("%02d", c.Month)
 
 	ye := strconv.Itoa(c.Year)
 
-	v1 := make(map[string]interface{})
+	var query struct {
+		CacheByMonth struct {
+			ID    graphql.ID       `graphql:"_id"`
+			Month graphql.String   `graphql:"month"`
+			Posts []graphql.String `graphql:"posts"`
+		} `graphql:"cacheByMonth(month: $month)"`
+	}
 
-	for k := q; k < 32; k++ {
+	v1 := map[string]interface{}{
+		"month": graphql.String(ye + `-` + mo),
+	}
 
-		value = ye + `-` + mo + `-` + fmt.Sprintf("%02d", k)
+	if err = call.Query(context.Background(), &query, v1); err != nil {
+		fmt.Fprintf(w, "get cache error: %v\n", err)
+	}
 
-		var q struct {
-			PostsByDate struct {
-				Data []Post
-			} `graphql:"postsByDate(date: $date iscommited: true)"`
+	result := query.CacheByMonth.Posts
+
+	hits := make(map[string]int, len(result))
+
+	if len(result) != 0 {
+
+		for _, v := range result {
+
+			m := strings.Split(strings.TrimPrefix(strings.TrimSuffix(string(v), `\"`), `\"`), ":")
+
+			if c, ok := hits[m[1]]; ok {
+
+				hits[m[1]] = c + 1
+
+			}
+
 		}
 
-		v1["date"] = graphql.String(value)
+	}
+
+	for k := q; k < 32; k++ {
 
 		switch c.Days[k] {
 
@@ -487,537 +452,195 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 				<span class="badge badge-pill badge-light">
 				<input readonly class="form-control-plaintext list-group-item-action" value="` + strconv.Itoa(k) + `" >
 				</span>
-				</button>
 				`
 
-			if err = call.Query(context.Background(), &q, v1); err != nil {
-				fmt.Fprint(w, err)
-			}
-
-			l := len(q.PostsByDate.Data)
-
-			if l > 0 {
-
-				for _, p := range q.PostsByDate.Data {
-
-					cache = append(cache, p.ID)
-
-				}
+			if l, ok := hits[ye+"-"+mo+"-"+fmt.Sprintf("%02d", k)]; ok {
 
 				str = str + `
-			<span style="text-align: inherit; color: #70db70" class="badge badge-pill badge-dark">
-			` + strconv.Itoa(l) + `
-			</span>
-			</button>
-			`
-
-			} else {
-
-				str = str + `
-			</button>
-			`
-				break
-
-			}
-
-			/* s := `{"query":"query{` + dir + `(date:\"` + value + `\" iscommited: true){data{_id}}}"}`
-			body := strings.NewReader(s)
-			req, _ := http.NewRequest("POST", "https://graphql.fauna.com/graphql", body)
-
-			req.Header.Set("Authorization", "Bearer "+access.Secret)
-			req.Header.Set("Content-Type", "application/json")
-			req.Header.Set("Accept", "application/json")
-			req.Header.Set("X-Schema-Preview", "partial-update-mutation")
-
-			resp, err := http.DefaultClient.Do(req)
-
-			if err != nil {
-
-				fmt.Fprint(w, err)
-
-				return
-
-			}
-
-			defer resp.Body.Close()
-
-			bdy, _ := ioutil.ReadAll(resp.Body)
-
-			var i interface{}
-
-			json.Unmarshal(bdy, &i)
-
-			if i == nil {
-
-				str = str + `
-				</button>
-				`
-
-				break
-
-			}
-
-			a := i.(map[string]interface{})
-
-			b := a["data"]
-
-			if b == nil {
-
-				str = str + `
-				</button>
-				`
-
-				break
-
-			}
-
-			c := b.(map[string]interface{})
-
-			d := c[dir]
-
-			if d == nil {
-
-				str = str + `
-				</button>
-				`
-
-				break
-
-			}
-
-			e := d.(map[string]interface{})
-
-			f := e["data"]
-
-			if f == nil {
-
-				str = str + `
-				</button>
-				`
-
-				break
-
-			}
-
-			g := f.([]interface{})
-
-			if g == nil {
-
-				str = str + `
-				</button>
-				`
-				break
-
-			} else {
-
-				h := make([]map[string]interface{}, len(g))
-
-				for j := 0; j < len(g); j++ {
-
-					h[j] = g[j].(map[string]interface{})
-
-				}
-
-				k := make(map[int]string, 0)
-
-				for j := 0; j < len(g); j++ {
-
-					k[j] = h[j]["_id"].(string)
-
-					if k[j] != "" {
-
-						cache = append(cache, k[j])
-
-					}
-
-				}
-
-				if len(k) > 0 {
-
-					str = str + `
 				<span style="text-align: inherit; color: #70db70" class="badge badge-pill badge-dark">
-				` + strconv.Itoa(len(k)) + `
+				` + strconv.Itoa(l) + `
 				</span>
-				</button>
 				`
 
-				} else {
+			}
 
-					str = str + `
-				</button>
-				`
-					break
-
-				}
-
-			} */
-
-			/* if string(b[k-q]) != "0" {
-				str = str + `
-				<span class="badge badge-dark">
-				` + string(b[k-q]) + `
-				</span>
-				</button>
-				`
-			} else {
-				str = str + `
-				</button>
-				`
-			} */
+			str = str + `
+			</button>
+			`
 
 		case "Tuesday":
 
 			str = str + `
-				<br>
-				<button type="button" class="btn btn-link" onclick="window.location.href='` + c.Days[k] + `'">
-				<span class="badge badge-pill badge-dark">
-				` + c.Days[k] + `
-				</span>
-				</button>
-				<button type="button" class="btn btn-light btn-link" onclick="window.location.href='entry#` + value + `'">
-				<span class="badge badge-pill badge-light">
-				<input readonly class="form-control-plaintext list-group-item-action" value="` + strconv.Itoa(k) + `" >
-				</span>
-				</button>
-				`
+			<br>
+			<button type="button" class="btn btn-link" onclick="window.location.href='` + c.Days[k] + `'">
+			<span class="badge badge-pill badge-dark">
+			` + c.Days[k] + `
+			</span>
+			</button>
+			<button type="button" class="btn btn-light btn-link" onclick="window.location.href='entry#` + value + `'">
+			<span class="badge badge-pill badge-light">
+			<input readonly class="form-control-plaintext list-group-item-action" value="` + strconv.Itoa(k) + `" >
+			</span>
+			`
 
-			if err = call.Query(context.Background(), &q, v1); err != nil {
-				fmt.Fprint(w, err)
-			}
-
-			l := len(q.PostsByDate.Data)
-
-			if l > 0 {
-
-				for _, p := range q.PostsByDate.Data {
-
-					cache = append(cache, p.ID)
-
-				}
+			if l, ok := hits[ye+"-"+mo+"-"+fmt.Sprintf("%02d", k)]; ok {
 
 				str = str + `
 			<span style="text-align: inherit; color: #70db70" class="badge badge-pill badge-dark">
 			` + strconv.Itoa(l) + `
 			</span>
-			</button>
 			`
-
-			} else {
-
-				str = str + `
-			</button>
-			`
-				break
 
 			}
+
+			str = str + `
+		</button>
+		`
 
 		case "Wednesday":
 
 			str = str + `
-				<br>
-				<button type="button" class="btn btn-link" onclick="window.location.href='` + c.Days[k] + `'">
-				<span class="badge badge-pill badge-dark">
-				` + c.Days[k] + `
-				</span>
-				</button>
-				<button type="button" class="btn btn-light btn-link" onclick="window.location.href='entry#` + value + `'">
-				<span class="badge badge-pill badge-light">
-				<input readonly class="form-control-plaintext list-group-item-action" value="` + strconv.Itoa(k) + `" >
-				</span>
-				</button>
-				`
+			<br>
+			<button type="button" class="btn btn-link" onclick="window.location.href='` + c.Days[k] + `'">
+			<span class="badge badge-pill badge-dark">
+			` + c.Days[k] + `
+			</span>
+			</button>
+			<button type="button" class="btn btn-light btn-link" onclick="window.location.href='entry#` + value + `'">
+			<span class="badge badge-pill badge-light">
+			<input readonly class="form-control-plaintext list-group-item-action" value="` + strconv.Itoa(k) + `" >
+			</span>
+			`
 
-			if err = call.Query(context.Background(), &q, v1); err != nil {
-				fmt.Fprint(w, err)
-			}
-
-			l := len(q.PostsByDate.Data)
-
-			if l > 0 {
-
-				for _, p := range q.PostsByDate.Data {
-
-					cache = append(cache, p.ID)
-
-				}
+			if l, ok := hits[ye+"-"+mo+"-"+fmt.Sprintf("%02d", k)]; ok {
 
 				str = str + `
 			<span style="text-align: inherit; color: #70db70" class="badge badge-pill badge-dark">
 			` + strconv.Itoa(l) + `
 			</span>
-			</button>
 			`
-
-			} else {
-
-				str = str + `
-			</button>
-			`
-				break
 
 			}
+
+			str = str + `
+		</button>
+		`
 
 		case "Thursday":
 
 			str = str + `
-				<br>
-				<button type="button" class="btn btn-link" onclick="window.location.href='` + c.Days[k] + `'">
-				<span class="badge badge-pill badge-dark">
-				` + c.Days[k] + `
-				</span>
-				</button>
-				<button type="button" class="btn btn-light btn-link" onclick="window.location.href='entry#` + value + `'">
-				<span class="badge badge-pill badge-light">
-				<input readonly class="form-control-plaintext list-group-item-action" value="` + strconv.Itoa(k) + `" >
-				</span>
-				</button>
-				`
+			<br>
+			<button type="button" class="btn btn-link" onclick="window.location.href='` + c.Days[k] + `'">
+			<span class="badge badge-pill badge-dark">
+			` + c.Days[k] + `
+			</span>
+			</button>
+			<button type="button" class="btn btn-light btn-link" onclick="window.location.href='entry#` + value + `'">
+			<span class="badge badge-pill badge-light">
+			<input readonly class="form-control-plaintext list-group-item-action" value="` + strconv.Itoa(k) + `" >
+			</span>
+			`
 
-			if err = call.Query(context.Background(), &q, v1); err != nil {
-				fmt.Fprint(w, err)
-			}
-
-			l := len(q.PostsByDate.Data)
-
-			if l > 0 {
-
-				for _, p := range q.PostsByDate.Data {
-
-					cache = append(cache, p.ID)
-
-				}
+			if l, ok := hits[ye+"-"+mo+"-"+fmt.Sprintf("%02d", k)]; ok {
 
 				str = str + `
 			<span style="text-align: inherit; color: #70db70" class="badge badge-pill badge-dark">
 			` + strconv.Itoa(l) + `
 			</span>
-			</button>
 			`
-
-			} else {
-
-				str = str + `
-			</button>
-			`
-				break
 
 			}
+
+			str = str + `
+		</button>
+		`
 
 		case "Friday":
 
 			str = str + `
-				<br>
-				<button type="button" class="btn btn-link" onclick="window.location.href='` + c.Days[k] + `'">
-				<span class="badge badge-pill badge-dark">
-				` + c.Days[k] + `
-				</span>
-				</button>
-				<button type="button" class="btn btn-light btn-link" onclick="window.location.href='entry#` + value + `'">
-				<span class="badge badge-pill badge-light">
-				<input readonly class="form-control-plaintext list-group-item-action" value="` + strconv.Itoa(k) + `" >
-				</span>
-				</button>
-				`
+			<br>
+			<button type="button" class="btn btn-link" onclick="window.location.href='` + c.Days[k] + `'">
+			<span class="badge badge-pill badge-dark">
+			` + c.Days[k] + `
+			</span>
+			</button>
+			<button type="button" class="btn btn-light btn-link" onclick="window.location.href='entry#` + value + `'">
+			<span class="badge badge-pill badge-light">
+			<input readonly class="form-control-plaintext list-group-item-action" value="` + strconv.Itoa(k) + `" >
+			</span>
+			`
 
-			if err = call.Query(context.Background(), &q, v1); err != nil {
-				fmt.Fprint(w, err)
-			}
-
-			l := len(q.PostsByDate.Data)
-
-			if l > 0 {
-
-				for _, p := range q.PostsByDate.Data {
-
-					cache = append(cache, p.ID)
-
-				}
+			if l, ok := hits[ye+"-"+mo+"-"+fmt.Sprintf("%02d", k)]; ok {
 
 				str = str + `
 			<span style="text-align: inherit; color: #70db70" class="badge badge-pill badge-dark">
 			` + strconv.Itoa(l) + `
 			</span>
-			</button>
 			`
-
-			} else {
-
-				str = str + `
-			</button>
-			`
-				break
 
 			}
+
+			str = str + `
+		</button>
+		`
 
 		case "Saturday":
 
 			str = str + `
-				<br>
-				<button type="button" class="btn btn-link" onclick="window.location.href='` + c.Days[k] + `'">
-				<span class="badge badge-pill badge-dark">
-				` + c.Days[k] + `
-				</span>
-				</button>
-				<button type="button" class="btn btn-light btn-link" onclick="window.location.href='entry#` + value + `'">
-				<span class="badge badge-pill badge-light">
-				<input readonly class="form-control-plaintext list-group-item-action" value="` + strconv.Itoa(k) + `" >
-				</span>
-				</button>
-				`
+			<br>
+			<button type="button" class="btn btn-link" onclick="window.location.href='` + c.Days[k] + `'">
+			<span class="badge badge-pill badge-dark">
+			` + c.Days[k] + `
+			</span>
+			</button>
+			<button type="button" class="btn btn-light btn-link" onclick="window.location.href='entry#` + value + `'">
+			<span class="badge badge-pill badge-light">
+			<input readonly class="form-control-plaintext list-group-item-action" value="` + strconv.Itoa(k) + `" >
+			</span>
+			`
 
-			if err = call.Query(context.Background(), &q, v1); err != nil {
-				fmt.Fprint(w, err)
-			}
-
-			l := len(q.PostsByDate.Data)
-
-			if l > 0 {
-
-				for _, p := range q.PostsByDate.Data {
-
-					cache = append(cache, p.ID)
-
-				}
+			if l, ok := hits[ye+"-"+mo+"-"+fmt.Sprintf("%02d", k)]; ok {
 
 				str = str + `
 			<span style="text-align: inherit; color: #70db70" class="badge badge-pill badge-dark">
 			` + strconv.Itoa(l) + `
 			</span>
-			</button>
 			`
-
-			} else {
-
-				str = str + `
-			</button>
-			`
-				break
 
 			}
+
+			str = str + `
+		</button>
+		`
 
 		case "Sunday":
 
 			str = str + `
-				<br>
-				<button type="button" class="btn btn-link" onclick="window.location.href='` + c.Days[k] + `'">
-				<span class="badge badge-pill badge-dark">
-				` + c.Days[k] + `
-				</span>
-				</button>
-				<button type="button" class="btn btn-light btn-link" onclick="window.location.href='entry#` + value + `'">
-				<span class="badge badge-pill badge-light">
-				<input readonly class="form-control-plaintext list-group-item-action" value="` + strconv.Itoa(k) + `" >
-				</span>
-				</button>
-				`
+			<br>
+			<button type="button" class="btn btn-link" onclick="window.location.href='` + c.Days[k] + `'">
+			<span class="badge badge-pill badge-dark">
+			` + c.Days[k] + `
+			</span>
+			</button>
+			<button type="button" class="btn btn-light btn-link" onclick="window.location.href='entry#` + value + `'">
+			<span class="badge badge-pill badge-light">
+			<input readonly class="form-control-plaintext list-group-item-action" value="` + strconv.Itoa(k) + `" >
+			</span>
+			`
 
-			if err = call.Query(context.Background(), &q, v1); err != nil {
-				fmt.Fprint(w, err)
-			}
-
-			l := len(q.PostsByDate.Data)
-
-			if l > 0 {
-
-				for _, p := range q.PostsByDate.Data {
-
-					cache = append(cache, p.ID)
-
-				}
+			if l, ok := hits[ye+"-"+mo+"-"+fmt.Sprintf("%02d", k)]; ok {
 
 				str = str + `
 			<span style="text-align: inherit; color: #70db70" class="badge badge-pill badge-dark">
 			` + strconv.Itoa(l) + `
 			</span>
-			</button>
 			`
 
-			} else {
-
-				str = str + `
-			</button>
-			`
-				break
-
 			}
 
-		}
-
-	}
-
-	if cache != nil {
-
-		var q struct {
-			CacheByMonth struct {
-				ID    graphql.ID       `graphql:"_id"`
-				Month graphql.String   `graphql:"month"`
-				Posts []graphql.String `graphql:"posts"`
-			} `graphql:"cacheByMonth(month: $month)"`
-		}
-
-		v2 := map[string]interface{}{
-			"month": graphql.String(ye + `-` + mo),
-		}
-
-		if err = call.Query(context.Background(), &q, v2); err != nil {
-			fmt.Fprintf(w, "get cache error: %v\n", err)
-		}
-
-		result := q.CacheByMonth
-
-		var posts []graphql.String = make([]graphql.String, 0)
-
-		if result.Posts == nil {
-
-			var m struct {
-				CreateCache struct {
-					ID    graphql.ID       `graphql:"_id"`
-					Month graphql.String   `graphql:"month"`
-					Posts []graphql.String `graphql:"posts"`
-				} `graphql:"createCache(data:{month: $month, posts: $posts})"`
-			}
-
-			for _, c := range cache {
-
-				posts = append(posts, c)
-			}
-
-			v3 := map[string]interface{}{
-				"month": v2["month"],
-				"posts": posts,
-			}
-
-			if err = call.Mutate(context.Background(), &m, v3); err != nil {
-				fmt.Fprintf(w, "create cache error: %v\n", err)
-			}
-
-		} else {
-
-			var m struct {
-				UpdateCache struct {
-					ID    graphql.ID       `graphql:"_id"`
-					Month graphql.String   `graphql:"month"`
-					Posts []graphql.String `graphql:"posts"`
-				} `graphql:"updateCache(id: $id, data:{month: $month, posts: $posts})"`
-			}
-
-			for _, c := range cache {
-
-				posts = append(posts, c)
-			}
-
-			//posts = append(posts, "253012617168159243")
-
-			v4 := map[string]interface{}{
-				"id":    result.ID,
-				"month": v2["month"],
-				"posts": posts,
-			}
-
-			if err = call.Mutate(context.Background(), &m, v4); err != nil {
-				fmt.Fprintf(w, "update cache error %v\n", err)
-			}
-
+			str = str + `
+		</button>
+		`
 		}
 
 	}
