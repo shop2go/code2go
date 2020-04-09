c1fcb8cc36f2e6be33ae968389e1c89824271fe6
mmaedel <medienwerksalzburg@gmail.com>
2020-04-09 17:58:39 +0200

    videonav

---
+++ video/index.go
@@ -0,0 +1,137 @@
+package main
+
+import (
+	//"context"
+	//"encoding/base64"
+	"fmt"
+	"net/http"
+	"os"
+	//"sort"
+	"strconv"
+	//"strings"
+
+	"github.com/muxinc/mux-go"
+	/* 	f "github.com/fauna/faunadb-go/faunadb"
+	   	"github.com/shurcooL/graphql" */
+	//"golang.org/x/oauth2"
+	//"github.com/plutov/paypal"
+)
+
+type Access struct {
+	//Reference *f.RefV `fauna:"ref"`
+	Timestamp int    `fauna:"ts"`
+	Secret    string `fauna:"secret"`
+	Role      string `fauna:"role"`
+}
+
+/* type ProductEntry struct {
+	ID      graphql.ID     `graphql:"_id"`
+	ImgURL  graphql.String `graphql:"imgURL"`
+	Product graphql.String `graphql:"product"`
+	Cat     graphql.String `graphql:"cat"`
+	Info    graphql.String `graphql:"info"`
+	Price   graphql.Float  `graphql:"price"`
+	Pack    graphql.Int    `graphql:"pack"`
+	InfoURL graphql.String `graphql:"infoURL"`
+	LinkURL graphql.String `graphql:"linkURL"`
+	LinkDIM graphql.Int    `graphql:"linkDIM"`
+}
+
+type CartEntry struct {
+	ID       graphql.ID   `graphql:"_id"`
+	Products []graphql.ID `graphql:"products"`
+}
+
+type SourceEntry struct {
+	ID     graphql.ID     `graphql:"_id"`
+	Link   graphql.ID     `graphql:"link"`
+	Origin graphql.String `graphql:"origin"`
+} */
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+
+	var s string
+
+	client := muxgo.NewAPIClient(
+		muxgo.NewConfiguration(
+			muxgo.WithBasicAuth(os.Getenv("MUX_ID"), os.Getenv("MUX_SECRET")),
+		))
+
+		car := muxgo.CreateAssetRequest{PlaybackPolicy: []muxgo.PlaybackPolicy{muxgo.PUBLIC}}
+		cur := muxgo.CreateUploadRequest{NewAssetSettings: car, Timeout: 3600, CorsOrigin: "code2go.dev"}
+		u, err := client.DirectUploadsApi.CreateDirectUpload(cur)
+
+		if err != nil {
+
+			fmt.Fprintf(w, "%v", err)
+
+		}
+		
+		s = u.Data.Url
+
+	switch r.Method {
+
+	case "GET":
+
+		str :=
+
+			`
+		<!DOCTYPE html>
+		<html lang="en">
+		<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<title>shop2go</title>
+		<!-- CSS -->
+		<!-- Add Material font (Roboto) and Material icon as needed -->
+		<link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700,700i|Roboto+Mono:300,400,700|Roboto+Slab:300,400,700" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+		<!-- Add Material CSS, replace Bootstrap CSS -->
+		<link href="https://assets.medienwerk.now.sh/material.min.css" rel="stylesheet">
+		</head>
+		<body style="background-color: #a1b116;">
+
+		<div class="container" id="shop" style="color:rgb(255, 255, 255); font-size:30px;">
+
+		<ul class="list-group">
+		<br>
+		<br>
+
+		<h1>Video</h1>
+
+		<form role="form" method="PUT">
+
+		<input id="file-picker" type="file" />
+
+		</form>
+
+		<script>	
+		const UpChunk = require('@mux/upchunk');
+
+		const filePicker = document.getElementById('file-picker');
+		
+		const url = 
+		
+		` + s + `
+		
+		filePicker.onchange = function () {
+		  const file = filePicker.files[0];
+		
+		  const upload = UpChunk.createUpload({
+			file,
+			endpoint: url
+		  });
+		  
+		  upload.on('success', () => console.log('file uploaded'));
+		  </script>
+		}`
+
+		w.Header().Set("Content-Type", "text/html")
+		w.Header().Set("Content-Length", strconv.Itoa(len(str)))
+		w.Write([]byte(str))
+
+	}
+
+}
