5a891fc3d3be7b2fff427010973872b385e2b554
mmaedel <medienwerksalzburg@gmail.com>
2020-04-16 12:03:14 +0200

    videonav

--- video/index.go
+++ video/index.go
@@ -107,6 +107,75 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 
 		dbID = m.CreateAsset.ID
 
+		assets, err := client.AssetsApi.ListAssets()
+
+		if err != nil {
+
+			fmt.Fprintf(w, "%v", err)
+
+		}
+
+		for _, a := range assets.Data {
+
+			input, _ := client.AssetsApi.GetAssetInputInfo(a.Id)
+
+			if input.Data != nil {
+
+				for _, b := range input.Data {
+
+					url := b.Settings.Url
+
+					url = strings.TrimPrefix(url, "https://storage.googleapis.com/video-storage-us-east1-uploads/")
+
+					sl := strings.SplitN(url, "?", 1)
+
+					//url = strings.TrimSuffix(sl[0], "?")
+
+					var q struct {
+						AssetBySourceID struct {
+							AssetEntry
+						} `graphql:"assetBySourceID(sourceID: $SourceID)"`
+					}
+
+					v := map[string]interface{}{
+						"SourceID": graphql.ID(sl[0]),
+					}
+
+					if err = caller.Query(context.Background(), &q, v); err != nil {
+						fmt.Printf("error with asset: %v\n", err)
+					}
+
+					if q.AssetBySourceID.ID == dbID {
+
+						var m struct {
+							UpdateAsset struct {
+								AssetEntry
+							} `graphql:"updateAsset(id: $ID, data:{assetID: $AssetID})"`
+						}
+
+						v = map[string]interface{}{
+							"ID":      q.AssetBySourceID.ID,
+							"AssetID": graphql.String(a.Id),
+						}
+
+						if err = caller.Mutate(context.Background(), &m, v); err != nil {
+							fmt.Printf("error with asset: %v\n", err)
+						}
+
+						break
+
+					}
+
+				}
+
+			} else {
+
+				continue
+
+			}
+
+		}
+
 	}
 
 	//http.NewRequest("PUT", s, nil)
@@ -230,76 +299,7 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Length", strconv.Itoa(len(str)))
 		w.Write([]byte(str))
 
-	case "POST":
-
-		assets, err := client.AssetsApi.ListAssets()
-
-		if err != nil {
-
-			fmt.Fprintf(w, "%v", err)
-
-		}
-
-		for _, a := range assets.Data {
-
-			input, _ := client.AssetsApi.GetAssetInputInfo(a.Id)
-
-			if input.Data != nil {
-
-				for _, b := range input.Data {
-
-					url := b.Settings.Url
-
-					url = strings.TrimPrefix(url, "https://storage.googleapis.com/video-storage-us-east1-uploads/")
-
-					sl := strings.SplitN(url, "?", 1)
-
-					//url = strings.TrimSuffix(sl[0], "?")
-
-					var q struct {
-						AssetBySourceID struct {
-							AssetEntry
-						} `graphql:"assetBySourceID(sourceID: $SourceID)"`
-					}
-
-					v := map[string]interface{}{
-						"SourceID": graphql.ID(sl[0]),
-					}
-
-					if err = caller.Query(context.Background(), &q, v); err != nil {
-						fmt.Printf("error with asset: %v\n", err)
-					}
-
-					if q.AssetBySourceID.ID == dbID {
-
-						var m struct {
-							UpdateAsset struct {
-								AssetEntry
-							} `graphql:"updateAsset(id: $ID, data:{assetID: $AssetID})"`
-						}
-
-						v = map[string]interface{}{
-							"ID":      q.AssetBySourceID.ID,
-							"AssetID": graphql.String(a.Id),
-						}
-
-						if err = caller.Mutate(context.Background(), &m, v); err != nil {
-							fmt.Printf("error with asset: %v\n", err)
-						}
-
-						break
-
-					}
-
-				}
-
-			} else {
-
-				continue
-
-			}
-
-		}
+	default:
 
 		if dbID != nil {
 
