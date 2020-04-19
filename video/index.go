1046a317679999e2f56154349697c8e7c91c0ec7
mmaedel <medienwerksalzburg@gmail.com>
2020-04-19 15:32:14 +0200

    videonav

--- video/index.go
+++ video/index.go
@@ -46,7 +46,7 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 
 	switch id {
 
-	case "SIGNED":
+	case "PUBLIC":
 
 		fc := f.NewFaunaClient(os.Getenv("FAUNA_ACCESS"))
 
@@ -77,19 +77,7 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 				muxgo.WithBasicAuth(os.Getenv("MUX_ID"), os.Getenv("MUX_SECRET")),
 			))
 
-		car := muxgo.CreateAssetRequest{}
-
-		switch id {
-
-		case "SIGNED":
-
-			car = muxgo.CreateAssetRequest{PlaybackPolicy: []muxgo.PlaybackPolicy{muxgo.SIGNED}}
-
-		default:
-
-			car = muxgo.CreateAssetRequest{PlaybackPolicy: []muxgo.PlaybackPolicy{muxgo.PUBLIC}}
-
-		}
+		car := muxgo.CreateAssetRequest{PlaybackPolicy: []muxgo.PlaybackPolicy{muxgo.PUBLIC}}
 
 		cur := muxgo.CreateUploadRequest{NewAssetSettings: car, Timeout: 3600, CorsOrigin: "code2go.dev"}
 
@@ -197,6 +185,146 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 			</html>
 					
 			`
+
+	case "SIGNED":
+
+		fc := f.NewFaunaClient(os.Getenv("FAUNA_ACCESS"))
+
+		x, err := fc.Query(f.CreateKey(f.Obj{"database": f.Database("assets"), "role": "server"}))
+
+		if err != nil {
+
+			fmt.Fprintf(w, "a connection error occured: %v\n", err)
+
+		}
+
+		var access *Access
+
+		x.Get(&access)
+
+		src := oauth2.StaticTokenSource(
+			&oauth2.Token{AccessToken: access.Secret},
+		)
+
+		access = &Access{}
+
+		httpClient := oauth2.NewClient(context.Background(), src)
+
+		caller := graphql.NewClient("https://graphql.fauna.com/graphql", httpClient)
+
+		client := muxgo.NewAPIClient(
+			muxgo.NewConfiguration(
+				muxgo.WithBasicAuth(os.Getenv("MUX_ID"), os.Getenv("MUX_SECRET")),
+			))
+
+		car := muxgo.CreateAssetRequest{PlaybackPolicy: []muxgo.PlaybackPolicy{muxgo.SIGNED}}
+
+		cur := muxgo.CreateUploadRequest{NewAssetSettings: car, Timeout: 3600, CorsOrigin: "code2go.dev"}
+
+		res, err := client.DirectUploadsApi.CreateDirectUpload(cur)
+
+		if err != nil {
+
+			fmt.Fprintf(w, "%s %v", "something went wrong...\n", err)
+
+		}
+
+		sourceURL := res.Data.Url
+
+		dul, _ := client.DirectUploadsApi.GetDirectUpload(res.Data.Id)
+
+		sourceID := dul.Data.Id
+
+		var m struct {
+			CreateAsset struct {
+				AssetEntry
+			} `graphql:"createAsset(data:{sourceID: $SourceID})"`
+		}
+
+		v := map[string]interface{}{
+			"SourceID": graphql.String(sourceID),
+		}
+
+		if err = caller.Mutate(context.Background(), &m, v); err != nil {
+			fmt.Fprintf(w, "error with asset source: %v\n", err)
+		}
+
+		i := fmt.Sprintf("%s", m.CreateAsset.ID)
+
+		content =
+
+			`
+				<!DOCTYPE html>
+				<html lang="en">
+				<head>
+				<meta charset="UTF-8">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				<meta http-equiv="X-UA-Compatible" content="ie=edge">
+				<title>vdo2go</title>
+				<!-- CSS -->
+				<!-- Add Material font (Roboto) and Material icon as needed -->
+				<link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700,700i|Roboto+Mono:300,400,700|Roboto+Slab:300,400,700" rel="stylesheet">
+				<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+				
+				<!-- Add Material CSS, replace Bootstrap CSS -->
+				<link href="https://assets.medienwerk.now.sh/material.min.css" rel="stylesheet">
+				
+				</head>
+				<body style="background-color: #a1b116;">
+				
+				<div class="container" id="video" style="color:rgb(255, 255, 255); font-size:30px;">
+				
+				<br>
+				<br>
+							
+				<h1>video upload:</h1>
+				<b>
+				<form>
+				<input id="picker" type="file" accept="video/*" />
+				<p>wait for the upload to complete => OK when prompted</p>
+				</form>		
+				
+				<form role="form" method="POST">
+							
+				<input readonly="true" class="form-control-plaintext" id="ID" aria-label="ID" name ="ID" value="` + i + `" hidden>
+				<br>
+							
+				<p>after file upload completion:</p>
+				<button type="submit" class="btn btn-light">submit</button>
+				
+				</form>
+				
+				</div>
+				<script src="https://unpkg.com/@mux/upchunk@1.0.6/dist/upchunk.js"></script>
+				
+				<script>
+		
+					const picker = document.getElementById('picker');
+					picker.onchange = () => {
+					  const endpoint = '` + sourceURL + `';
+					  const file = picker.files[0];
+					
+					  const upload = UpChunk.createUpload({
+						endpoint,
+						file,
+						chunkSize: 5120,
+					  });
+					  upload.on('error', err => {
+						console.error('something went wrong', err.detail);
+					  });				  
+					
+					  upload.on('success', () => {
+						alert('video file upload completed.');
+					  });
+					};
+					 
+				</script>
+				
+				<script src="https://assets.medienwerk.now.sh/material.min.js"></script>
+				</body>
+				</html>
+						
+				`
 
 	case "":
 
@@ -358,13 +486,13 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 
 		switch id {
 
-		case "SIGNED":
+		case "PUBLIC":
 
 			w.Header().Set("Content-Type", "text/html")
 			w.Header().Set("Content-Length", strconv.Itoa(len(content)))
 			w.Write([]byte(content))
 
-		default:
+		case "SIGNED":
 
 			w.Header().Set("Content-Type", "text/html")
 			w.Header().Set("Content-Length", strconv.Itoa(len(content)))
@@ -419,11 +547,26 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 			w.Header().Set("Content-Length", strconv.Itoa(len(content)))
 			w.Write([]byte(content))
 
+		default:
+
+			w.Header().Set("Content-Type", "text/html")
+			w.Header().Set("Content-Length", strconv.Itoa(len(content)))
+			w.Write([]byte(content))
+
 		}
 
 	case "POST":
 
 		switch id {
+
+		case "PUBLIC":
+
+			r.ParseForm()
+
+			id = r.Form.Get("ID")
+
+			//fmt.Fprintf(w, "id: %v\n", i)
+			http.Redirect(w, r, "https://"+id+".code2go.dev/video", http.StatusSeeOther)
 
 		case "SIGNED":
 
@@ -442,8 +585,11 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 
 			if c != "" {
 
-				c = c + "."
+				c = strings.ToUpper(c) + "."
 
+			} else {
+
+				c = "PUBLIC."
 			}
 
 			http.Redirect(w, r, "https://"+c+"code2go.dev/video", http.StatusSeeOther)
